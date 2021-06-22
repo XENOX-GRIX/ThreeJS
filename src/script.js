c6c77102f69aba408d37bca068592cc282a3dc07
Rajanyo Paul <xenoxgrik@gmail.com>
2021-06-22 20:05:07 +0530

    Created a basic model of a planetary system with 3 planets

--- src/script.js
+++ src/script.js
@@ -1,43 +1,94 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js'
+
 import gsap from 'gsap'
 import * as dat from 'dat.gui'
+// Textures 
 
-/**
- * Base
- */
-// Canvas
+const texturLoader = new THREE.TextureLoader()
+const alpha=texturLoader.load('/textures/door/alpha.jpg')
+const sun=texturLoader.load('/textures/door/sun.jpg')
+const color=texturLoader.load('/textures/door/color.jpg')
+const earth=texturLoader.load('/textures/door/earth.png')
+const metalness=texturLoader.load('/textures/door/metalness.jpg')
+const normal=texturLoader.load('/textures/door/normal.jpg')
+const planet3=texturLoader.load('/textures/door/planet2.jpg')
+const rock=texturLoader.load('/textures/door/rock.jpg')
+const planet1=texturLoader.load('/textures/door/planet1.jpg')
+const backgroung=texturLoader.load('/textures/door/backgroung.jpg')
+// // 
+//  * Base
+//  */
+// // Canvas
 
-const gui = new dat.GUI({closed:true,width:400})
-const debugParam={
-    color:0x2adbc0,
-    spin:()=>{
-        gsap.to(mesh.rotation,{duration:1, y:mesh.rotation.y+50*(Math.PI+1)})
-    }
-}
+// const gui = new dat.GUI({closed:true,width:400})
+// const debugParam={
+//     color:0xfffbfb,
+//     spin:()=>{
+//         gsap.to(mesh.rotation,{duration:1,delay:0.2, y:mesh.rotation.y+50*(Math.PI+1)})
+//     }
+// }
 
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+const geometry = new THREE.SphereGeometry(20,32,32);
+const material = new THREE.MeshBasicMaterial( {map:planet1} );
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
+
+const geometry2 = new THREE.SphereGeometry(15,32,32);
+const material2 = new THREE.MeshBasicMaterial( {map:earth} );
+const mesh2 = new THREE.Mesh(geometry2,material2);
+scene.add(mesh2);
+
+const geometry3 = new THREE.SphereGeometry(200,32,32);
+const material3 = new THREE.MeshBasicMaterial( {map:sun} );
+const sunn = new THREE.Mesh(geometry3,material3);
+scene.add(sunn);
+
+const geometry4 = new THREE.SphereGeometry(45,32,32);
+const material4 = new THREE.MeshBasicMaterial( {map:planet3} );
+const planet2 = new THREE.Mesh(geometry4,material4);
+scene.add(planet2);
+
+// const geometry_sun = new THREE.SphereGeometry(20,32,32);
+// const material_sun = new THREE.MeshBasicMaterial( {map:sun} );
+// const sun = new THREE.Mesh(geometry_sun,material_sun);
+// scene.add(sun);
+scene.background= backgroung
+
+
+mesh2.position.set(30,0,0)
+mesh.position.set(0,0,0)
+sunn.position.set(-150,0,0)
+planet2.position.set(100,0,0)
+// sun.position.set(-30,0,0)
+
+
+
 
 /**
  * Object
  */
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: debugParam.color })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-//debug
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({map:texture})
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
-gui.add(mesh.position,'y',-1,1,0.01)
-gui
-    .addColor(debugParam,"color")
-    .onChange(()=>{
-        material.color.set(debugParam.color)
-    })
-gui.add(debugParam,"spin")
+
+// debug
+
+// gui.add(mesh.position,'y',-1,1,0.01)
+// gui
+//     .addColor(debugParam,"color")
+//     .onChange(()=>{
+//         material.color.set(debugParam.color)
+//     })
+// gui.add(debugParam,"spin")
 
 /**
  * Sizes
@@ -66,13 +117,24 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 50000)
+camera.position.z = 800
+
 scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
+const controls = new TrackballControls(camera, canvas)
 controls.enableDamping = true
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1;
+// controls.panSpeed = 0.8;
+// controls.keys = [65, 83, 68]
+// controls.noPan = true //default false
+// controls.noRotate = true //default false
+// controls.noZoom = true //default false
+// controls.staticMoving = true //default false
+ controls.maxDistance = 10000;
+// controls.minDistance = 2;
 
 /**
  * Renderer
@@ -91,8 +153,20 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    gsap.to(mesh.rotation,{y:elapsedTime})
+    gsap.to(mesh2.rotation,{y:elapsedTime})
+    gsap.to(sunn.rotation,{y:elapsedTime/20})
+    gsap.to(planet2.rotation,{y:elapsedTime/3})
 
-    // Update controls
+    mesh2.position.z=Math.sin(elapsedTime/3)*540
+    mesh2.position.x=Math.cos(elapsedTime/3)*550
+
+    mesh.position.z=Math.sin(elapsedTime/5)*700
+    mesh.position.x=Math.cos(elapsedTime/5)*750
+
+    planet2.position.z=Math.sin(elapsedTime/10)*1000
+    planet2.position.x=Math.cos(elapsedTime/10)*950
+
     controls.update()
 
     // Render
